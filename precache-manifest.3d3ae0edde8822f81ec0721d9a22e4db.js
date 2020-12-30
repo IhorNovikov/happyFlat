402ec55e1e967129b774d00a3ca45d29104a30be
Ihor Novikov <novikov8118@gmail.com>
2020-12-30 11:44:24 +0200

    Updates

---
+++ precache-manifest.3d3ae0edde8822f81ec0721d9a22e4db.js
@@ -0,0 +1,290 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0164b3451035de4c26d2caf83058ff2",
+    "url": "/happyFlat/index.html"
+  },
+  {
+    "revision": "4b93eb4da0f790fb910e",
+    "url": "/happyFlat/static/css/main.1ca9c161.chunk.css"
+  },
+  {
+    "revision": "fad4d9ee85076c61dbea",
+    "url": "/happyFlat/static/js/2.0d7d4584.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/happyFlat/static/js/2.0d7d4584.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b93eb4da0f790fb910e",
+    "url": "/happyFlat/static/js/main.f0a81c16.chunk.js"
+  },
+  {
+    "revision": "9131d9f85a14cf0f5bda",
+    "url": "/happyFlat/static/js/runtime-main.cd69d672.js"
+  },
+  {
+    "revision": "0d212a83ede438fde4e5f30f3e1039a5",
+    "url": "/happyFlat/static/media/1.0d212a83.png"
+  },
+  {
+    "revision": "13dfb46bbcfc499cbcbc7eb55ee8eff7",
+    "url": "/happyFlat/static/media/1.13dfb46b.jpg"
+  },
+  {
+    "revision": "256adbf44562f5123e9541ef48ea1ec3",
+    "url": "/happyFlat/static/media/1.256adbf4.jpg"
+  },
+  {
+    "revision": "25a6f553ea287a06aad75bffe111d9b0",
+    "url": "/happyFlat/static/media/1.25a6f553.jpg"
+  },
+  {
+    "revision": "2698cc8732c3b9c6df50b4fdc57b7c2e",
+    "url": "/happyFlat/static/media/1.2698cc87.jpg"
+  },
+  {
+    "revision": "2aacd2f1e5fa705d3869170bd14fad6a",
+    "url": "/happyFlat/static/media/1.2aacd2f1.jpg"
+  },
+  {
+    "revision": "491a3fdc20f158a4203101ae3d34be20",
+    "url": "/happyFlat/static/media/1.491a3fdc.jpg"
+  },
+  {
+    "revision": "6a49a1835659f66c37629ff85aa83f14",
+    "url": "/happyFlat/static/media/1.6a49a183.jpg"
+  },
+  {
+    "revision": "818b8f2a0dc4701c6d8b145ac6530e39",
+    "url": "/happyFlat/static/media/1.818b8f2a.jpg"
+  },
+  {
+    "revision": "8699e12c18e301cb873765e85a9331c8",
+    "url": "/happyFlat/static/media/1.8699e12c.jpg"
+  },
+  {
+    "revision": "c7dfd65962f19d9e2843736d4e37ec90",
+    "url": "/happyFlat/static/media/1.c7dfd659.jpg"
+  },
+  {
+    "revision": "0bac55080ee40ae7d4f2febfe4abaf4a",
+    "url": "/happyFlat/static/media/2.0bac5508.jpg"
+  },
+  {
+    "revision": "383a49bddf67d6e597e885182c73149c",
+    "url": "/happyFlat/static/media/2.383a49bd.jpg"
+  },
+  {
+    "revision": "5787653f9df6fc851fc51d258ace7fa7",
+    "url": "/happyFlat/static/media/2.5787653f.jpg"
+  },
+  {
+    "revision": "6f93da582092b1bfe88264950d9723af",
+    "url": "/happyFlat/static/media/2.6f93da58.jpg"
+  },
+  {
+    "revision": "8b85c6f5100af6b4143f115322103773",
+    "url": "/happyFlat/static/media/2.8b85c6f5.jpg"
+  },
+  {
+    "revision": "8e32fd4f3052e6bfbb961ed6f9b91508",
+    "url": "/happyFlat/static/media/2.8e32fd4f.jpg"
+  },
+  {
+    "revision": "b3c7062d4bf52725838cab9190c23ae2",
+    "url": "/happyFlat/static/media/2.b3c7062d.jpg"
+  },
+  {
+    "revision": "bcef0571999fc96dcead50454f75a047",
+    "url": "/happyFlat/static/media/2.bcef0571.jpg"
+  },
+  {
+    "revision": "cb1e6367d8d8f2761e2df48149c5c66e",
+    "url": "/happyFlat/static/media/2.cb1e6367.jpg"
+  },
+  {
+    "revision": "d7061fa87864dfc69a85d83187f72bad",
+    "url": "/happyFlat/static/media/2.d7061fa8.jpg"
+  },
+  {
+    "revision": "f7c088d896fe5b839350a932bcf43edd",
+    "url": "/happyFlat/static/media/2.f7c088d8.png"
+  },
+  {
+    "revision": "42850da5269fa91a97c4af9f062ce929",
+    "url": "/happyFlat/static/media/3.42850da5.jpg"
+  },
+  {
+    "revision": "5c2ff4f012912dcf3e46da5415b79732",
+    "url": "/happyFlat/static/media/3.5c2ff4f0.jpg"
+  },
+  {
+    "revision": "661cc327659a74a1092c443c361576aa",
+    "url": "/happyFlat/static/media/3.661cc327.jpg"
+  },
+  {
+    "revision": "72e077ccae5eac670a0aeaaf4073c034",
+    "url": "/happyFlat/static/media/3.72e077cc.jpg"
+  },
+  {
+    "revision": "7d19dd95d1ade4498c8546888f644ed3",
+    "url": "/happyFlat/static/media/3.7d19dd95.jpg"
+  },
+  {
+    "revision": "8fd0ff49de5dbd57c530bc9dacd1fe8f",
+    "url": "/happyFlat/static/media/3.8fd0ff49.png"
+  },
+  {
+    "revision": "ac397c1a7b89d3ee603dbb37a3f9ebde",
+    "url": "/happyFlat/static/media/3.ac397c1a.jpg"
+  },
+  {
+    "revision": "d9bd546dc5180048aed7210ce0d75d5d",
+    "url": "/happyFlat/static/media/3.d9bd546d.jpg"
+  },
+  {
+    "revision": "e5614e18d8f6e48e3bf6c442607b100f",
+    "url": "/happyFlat/static/media/3.e5614e18.jpg"
+  },
+  {
+    "revision": "f69b59f9e8eef9482e4275f59f886d6b",
+    "url": "/happyFlat/static/media/3.f69b59f9.jpg"
+  },
+  {
+    "revision": "3c7eb925c2937564077d3b0b91ae9551",
+    "url": "/happyFlat/static/media/4.3c7eb925.jpg"
+  },
+  {
+    "revision": "3d89415bc18d839c5f95de245ca9d728",
+    "url": "/happyFlat/static/media/4.3d89415b.jpg"
+  },
+  {
+    "revision": "7c00f1634f74a31c6c719a7b4e663770",
+    "url": "/happyFlat/static/media/4.7c00f163.jpg"
+  },
+  {
+    "revision": "9cad580f5ba3a90bbe72ff20d3a9cbd3",
+    "url": "/happyFlat/static/media/4.9cad580f.jpg"
+  },
+  {
+    "revision": "d490e7d3c4cac3e8140c62e4bc8b31a9",
+    "url": "/happyFlat/static/media/4.d490e7d3.jpg"
+  },
+  {
+    "revision": "d93ffb24443cfe3fb7f8d6534d1decfc",
+    "url": "/happyFlat/static/media/4.d93ffb24.jpg"
+  },
+  {
+    "revision": "18775964b0ddcafa5f6c09d128cf2218",
+    "url": "/happyFlat/static/media/5.18775964.jpg"
+  },
+  {
+    "revision": "946d8745a63aa50f1e65db32acc4fc93",
+    "url": "/happyFlat/static/media/5.946d8745.jpg"
+  },
+  {
+    "revision": "a9c76bfdb5e244300e60093b29944321",
+    "url": "/happyFlat/static/media/5.a9c76bfd.jpg"
+  },
+  {
+    "revision": "b404200a7c6d0fdb18d035c4a91e7e55",
+    "url": "/happyFlat/static/media/5.b404200a.jpg"
+  },
+  {
+    "revision": "c16006b1c52438cec502cf8a69bac864",
+    "url": "/happyFlat/static/media/5.c16006b1.jpg"
+  },
+  {
+    "revision": "c89fbdf618787cc8b660953f9169d5af",
+    "url": "/happyFlat/static/media/5.c89fbdf6.jpg"
+  },
+  {
+    "revision": "c868393a918688b94b48f17152d4e8c7",
+    "url": "/happyFlat/static/media/6.c868393a.jpg"
+  },
+  {
+    "revision": "e0ce220eac3490ca185d923dc0ab40d4",
+    "url": "/happyFlat/static/media/6.e0ce220e.jpg"
+  },
+  {
+    "revision": "625e3412c97189a26c013e5478679d44",
+    "url": "/happyFlat/static/media/7.625e3412.jpg"
+  },
+  {
+    "revision": "f1421e5e3c70a46737a5ff474adf2d65",
+    "url": "/happyFlat/static/media/7.f1421e5e.jpg"
+  },
+  {
+    "revision": "97dcc351640e5432afbbc45997594af0",
+    "url": "/happyFlat/static/media/Firefox_Developer.97dcc351.png"
+  },
+  {
+    "revision": "256adbf44562f5123e9541ef48ea1ec3",
+    "url": "/happyFlat/static/media/article1.256adbf4.jpg"
+  },
+  {
+    "revision": "7ebc8ecf65e753b23dc37e4bcc26ef9c",
+    "url": "/happyFlat/static/media/article2.7ebc8ecf.jpg"
+  },
+  {
+    "revision": "f1421e5e3c70a46737a5ff474adf2d65",
+    "url": "/happyFlat/static/media/article3.f1421e5e.jpg"
+  },
+  {
+    "revision": "b3c7062d4bf52725838cab9190c23ae2",
+    "url": "/happyFlat/static/media/article4.b3c7062d.jpg"
+  },
+  {
+    "revision": "bcef0571999fc96dcead50454f75a047",
+    "url": "/happyFlat/static/media/article5.bcef0571.jpg"
+  },
+  {
+    "revision": "a5136f35fbcf2ccbeb8198d3cee3a301",
+    "url": "/happyFlat/static/media/article6.a5136f35.jpg"
+  },
+  {
+    "revision": "b3f29c8f6198aa6d6875d73cc18aef83",
+    "url": "/happyFlat/static/media/background2.b3f29c8f.jpg"
+  },
+  {
+    "revision": "2ad99944ab6b8bc02774e11483eea184",
+    "url": "/happyFlat/static/media/contacts.2ad99944.jpg"
+  },
+  {
+    "revision": "e37a5e9cc019e123651c7d57fb8a045b",
+    "url": "/happyFlat/static/media/git.e37a5e9c.png"
+  },
+  {
+    "revision": "0c60ec74492ed4fdea062e75ce72584a",
+    "url": "/happyFlat/static/media/html5.0c60ec74.png"
+  },
+  {
+    "revision": "6df411d1b9d5c631c79af3a33dfc1760",
+    "url": "/happyFlat/static/media/js.6df411d1.png"
+  },
+  {
+    "revision": "56f86776503875fc35f0ffc52af3a4a9",
+    "url": "/happyFlat/static/media/lupa2.56f86776.jpg"
+  },
+  {
+    "revision": "be00fc4a29d03016e78b28c9943e3f51",
+    "url": "/happyFlat/static/media/oval.be00fc4a.svg"
+  },
+  {
+    "revision": "b4e23a502e44692d9f3c8b427ac23e01",
+    "url": "/happyFlat/static/media/redux.b4e23a50.png"
+  },
+  {
+    "revision": "794d10add629641fa4868ed80dcb2658",
+    "url": "/happyFlat/static/media/roadMap.794d10ad.png"
+  },
+  {
+    "revision": "0d7217eed95e423613ea38b8d6ff3875",
+    "url": "/happyFlat/static/media/room5.0d7217ee.jpg"
+  },
+  {
+    "revision": "aed21612829cf0a2bd6809432ac8fe49",
+    "url": "/happyFlat/static/media/team.aed21612.jpg"
+  }
+]);
